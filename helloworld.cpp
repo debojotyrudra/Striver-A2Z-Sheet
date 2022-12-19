8baefa042c2a17c1462fbf43ff7e5895c6510d6b
debojotyrudra <87115796+debojotyrudra@users.noreply.github.com>
2022-12-19 21:46:55 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //cout<<"Hello world!"<<endl;
+    int x;
+    cout<<"Hello please enter a value!"<<endl;
+    cin>>x;
+    cout<<"Value="<<x<<endl;
+    return 0;
+}
